f80cc8456de7677076527c800de9c3d4cb385819
Raj Patel <rajpatel8690@gmail.com>
2021-07-27 18:27:30 +0530

    fix bug

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
-    if (route === 'signout') {
+    if (route === 'signout' || route === 'signin') {
       this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
